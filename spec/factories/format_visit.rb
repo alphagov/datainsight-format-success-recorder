cb20ac6007d82d00f20293a2d916fcdcd4ae2fbc
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-19 12:13:57 +0100

    Add integration test for the web app.
    author: @jthoenes

--- spec/factories/format_visit.rb
+++ spec/factories/format_visit.rb
@@ -3,8 +3,8 @@ require_relative "../../lib/datamapper_config"
 FactoryGirl.define do
   factory :format_visits, class: FormatVisits do
         collected_at DateTime.new(2012, 9, 18, 11, 42, 23)
-        start_at DateTime.new(2012, 9, 9)
-        end_at DateTime.new(2012, 9, 15)
+        start_at Date.new(2012, 9, 9)
+        end_at Date.new(2012, 9, 15)
         entries 5000
         successes 4000
         format 'transaction'
--- spec/integration/format_success_web_spec.rb
+++ spec/integration/format_success_web_spec.rb
@@ -7,8 +7,24 @@ describe("Format Success Web") do
     Sinatra::Application
   end
 
-  after(:each) do
+  it "should return the JSON data" do
+    FactoryGirl.create(:format_visits, :format => 'guide')
+    FactoryGirl.create(:format_visits, :format => 'transactions')
+    FactoryGirl.create(:format_visits, :format => 'smart_answers')
 
+    get '/format-success'
+
+    last_response.should be_ok
+    response = JSON.parse(last_response.body)
+
+    response.should be_an(Array)
+    response.should include('format' => 'Guide', 'entries' => 5000, 'percentage_of_success' => 80.0)
+    response.should include('format' => 'Transactions', 'entries' => 5000, 'percentage_of_success' => 80.0)
+    response.should include('format' => 'Smart Answers', 'entries' => 5000, 'percentage_of_success' => 80.0)
+  end
+
+  after(:each) do
+    FormatVisits.destroy!
   end
 
 
